4ca58e653ebf359cdad90097d3ee639161ed1063
quentinBOSSUS <quentplop@gmail.com>
2026-02-13 16:00:22 +0100

    test(tests): add tests

---
+++ tests/math.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+describe('Tests de base ', () => {
+  it('Le résultat de 1 + 1 = 2', () => {
+    const resultat = 1 + 1
+    expect(resultat).toBe(2)
+  })
+})
